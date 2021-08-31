6d2142c7449ab01a0ac44685905a69b77474e7ee
Myuze <Myuze@users.noreply.github.com>
2021-08-30 18:13:43 -0700

    Added tests for other classes.

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,24 +1,53 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
+  const memName = 'Gorge';
+  const memId = 1;
+  const memEmail = 'gorge@gmail.com';
+
+  const member = new Employee(memName, memId, memEmail);
+
   describe('Properties', () => {
-    
     it('Should have the property "name", as a string', () => {
-      const member = new Employee('Gorge', 1, 'gorge@gmail.com');
       expect(member.name).toEqual('Gorge');
       expect(typeof(member.name)).toBe('string');
     });
     
     it('Should have the property "id", as a number', () => {
-      const member = new Employee('Gorge', 1, 'gorge@gmail.com');
       expect(member.id).toEqual(1);
       expect(typeof(member.id)).toBe('number');
     });
     
     it('Should have the property "email", as a string', () => {
-      const member = new Employee('Gorge', 1, 'gorge@gmail.com');
       expect(member.email).toEqual('gorge@gmail.com');
       expect(typeof(member.email)).toBe('string');
+    });
+  });
+
+  describe('Methods', () => {
+    it('Should have the method "getName", which returns the "name" value.', () => {
+      const result = member.getName();
+
+      expect(result).toEqual(memName);
+    });
+
+    it('Should have the method "getId", which returns the "id" value.', () => {
+      const result = member.getId();
+
+      expect(result).toEqual(memId);
+    });
+
+    it('Should have the method "getEmail", which returns the "email" value.', () => {
+      const result = member.getEmail(memEmail);
+
+      expect(result).toEqual(memEmail);
+    });
+    
+    it('Should have the method "getRole", which returns the role "Employee" value, as a string.', () => {
+      const result = member.getRole();
+
+      expect(result).toEqual('Employee');
+      expect(typeof(result)).toBe('string');
     });
   });
 });
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,11 +1,53 @@
 const Manager = require('../lib/Manager');
 
 describe('Manager', () => {
+  const memName = 'Shigu';
+  const memId = 1;
+  const memEmail = 'shigu@gmail.com';
+
+  const member = new Manager(memName, memId, memEmail);
+
   describe('Properties', () => {
-    const member = new Manager('Shigu', 2, 'shigu@gmail.com');
     it('Should have the property "name", as a string', () => {
       expect(member.name).toEqual('Shigu');
       expect(typeof(member.name)).toBe('string');
+    });
+    
+    it('Should have the property "id", as a number', () => {
+      expect(member.id).toEqual(1);
+      expect(typeof(member.id)).toBe('number');
+    });
+    
+    it('Should have the property "email", as a string', () => {
+      expect(member.email).toEqual('shigu@gmail.com');
+      expect(typeof(member.email)).toBe('string');
+    });
+  });
+
+  describe('Methods', () => {
+    it('Should have the method "getName", which returns the "name" value.', () => {
+      const result = member.getName();
+
+      expect(result).toEqual(memName);
+    });
+
+    it('Should have the method "getId", which returns the "id" value.', () => {
+      const result = member.getId();
+
+      expect(result).toEqual(memId);
+    });
+
+    it('Should have the method "getEmail", which returns the "email" value.', () => {
+      const result = member.getEmail(memEmail);
+
+      expect(result).toEqual(memEmail);
+    });
+    
+    it('Should have the method "getRole", which returns the role "Manager" value, as a string.', () => {
+      const result = member.getRole();
+
+      expect(result).toEqual('Manager');
+      expect(typeof(result)).toBe('string');
     });
   });
 });
