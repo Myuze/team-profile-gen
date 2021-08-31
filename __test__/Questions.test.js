00f7e31f53c6df2e4166e0baa5a579a6fe831a58
Myuze <Myuze@users.noreply.github.com>
2021-08-30 23:07:14 -0700

    Added Questions test

---
+++ __test__/Questions.test.js
@@ -0,0 +1,9 @@
+const Questions = require('../src/Questions');
+
+describe('Questions', () => {
+  describe('functions', () => {
+    // It Prompts Questions needed for application
+
+    // It Sends responses to be injected into HTML creator
+  });
+});
