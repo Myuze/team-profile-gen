fde12f8c81f3bea61444b44922f2f25379a26ca5
Florian Meneses <florian.meneses@gmail.com>
2021-09-10 17:09:30 -0700

    Styling updates

--- src/RenderHTML.js
+++ src/RenderHTML.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 class RenderHTML {
   constructor() {
-    this.headHTML = `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<link rel="preconnect" href="https://fonts.googleapis.com">\n\t<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n\t<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">\n\t<link rel="stylesheet" href="styles.css">\n\t<title>Team Profil</title>\n</head>\n`;
+    this.headHTML = `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<link rel="preconnect" href="https://fonts.googleapis.com">\n\t<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n\t<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Michroma&display=swap" rel="stylesheet">\n\t<link rel="stylesheet" href="styles.css">\n\t<title>Team Profile</title>\n</head>\n`;
     this.bodyHTML = ``;
     this.endHTML = `\n</html>`;
     this.completeHTML = ``;
